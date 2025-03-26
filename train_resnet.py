f012412b6c8d59c6a7eb3346c7a59e0a3757c8ad
Max May <max.may@ziggo.nl>
2025-03-26 14:51:41 +0100

    add train_loop

--- train_resnet.py
+++ train_resnet.py
@@ -1,16 +1,44 @@
 import numpy as np
+
 import torch
+from torch.utils.data import DataLoader
+import torch.optim as optim 
+import torch.nn as nn
 import torchsummary
 
-from models.resnet import ResNet
+from models.resnet import ResNet, Bottleneck
+from dataloader.dataloader import AudioDataset
 
 import sofa
+from tqdm import tqdm
 
 
-def train(model):
-    acc = 0
-    loss = 0
-    return acc, loss
+def train(model, dataloader, criterion, scheduler, optimizer, device):
+    model.train()
+
+    train_running_loss = 0.0
+    train_running_acc = 0
+    for idx, (data, labels) in enumerate(tqdm(dataloader, desc=f'Training')):
+        data = data.to(device, dtype=torch.float)
+
+        outputs = model(data)
+
+        # Calculate loss
+        labels = labels.to(device)
+        loss = criterion(outputs, labels)
+        train_running_loss += loss.item()
+
+        # Calculate accuracy
+        _, preds = torch.max(outputs.data, 1)
+        train_running_acc += (preds == labels).sum().item()
+
+        loss.backward()
+        optimizer.step()
+
+    scheduler.step()
+    epoch_loss = train_running_loss / len(dataloader)
+    epoch_acc = 100. * (train_running_acc / len(dataloader.dataset))
+    return epoch_acc, epoch_loss
 
 
 @torch.no_grad()
@@ -22,19 +50,44 @@ def eval(model):
 
 
 def main():
-    nr_epochs = 1
+    nr_epochs = 5
+    batch_size = 2
 
-    resnet = ResNet()
+    # CUDA for PyTorch
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+    torch.backends.cudnn.benchmark = True
+    print(f'Using device: "{device}"')
+
+    # Model
+    print('Building model...')
+    model = ResNet(block=Bottleneck, layers=[3,4,6,3], input_channels=2, num_classes=828)
+    model = model.to(device)
+
+    # Optimizer
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones = [40,60], gamma=.75) # 60 is added in case a higher number of epochs is used
+    
+    # Loss function.
+    criterion = nn.CrossEntropyLoss()
+
+    train_data = AudioDataset(
+                    dir='/home/maxmay/files_to_copy.txt', 
+                    target_samplerate = 48000
+                )
+    # test_data = AudioDataset('test')
+
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    # test_loader = DataLoader(test_data)
+
     # torchsummary.summary(resnet, (3, 128, 128))
 
     for epoch in range(nr_epochs):
-        acc, loss = train(resnet)
-        acc, loss = eval(resnet)
+        print(f'Epoch:[{epoch+1}/{nr_epochs}]')
+        acc, loss = train(model, train_loader, criterion, scheduler, optimizer, device)
+        print(f'Accuracy: {acc}\nLoss: {loss}\n')
+        # acc, loss = test(resnet, test_loader)
 
 
 if __name__ == "__main__":
     main()
-    # hrtf = sofa.Database.open("./utils/KEMAR_Knowl_EarSim_SmallEars_FreeFieldComp_48kHz.sofa")
-    # hrtf.Metadata.dump()
-    # source_positions = hrtf.Source.Position.get_values(system="cartesian")
-    # print(np.asarray(source_positions).shape)
